925fb450bc0d719a2d94c2450625863fc3044de3
Gerrard <catdogscat@gmail.com>
2019-07-31 15:50:40 +0900

    add express server module

--- client/server.js
+++ client/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!!!'});
-});
+    res.send({message: 'Hello Express!'});
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
