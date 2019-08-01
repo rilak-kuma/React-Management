1851cd30beba6ebb972ce6e3af277e8e2d71942d
Gerrard <catdogscat@gmail.com>
2019-08-02 00:33:57 +0900

    react mysql connection

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import circularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
@@ -17,6 +19,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
 
@@ -49,10 +54,22 @@ const styles = theme => ({
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
+  /*
+  component Life Cycle
+  1) constructor()
+  2) componentWillMount()
+  3) render()
+  4) componentDidMount()
+  */
+  /*
+  props or state => shouldComponentUpdate()
+  */
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -62,6 +79,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render() {
@@ -95,7 +117,13 @@ class App extends Component {
               />
             );
 
-          }) : "" }
+          }) : 
+          <TableRow>
+            <TableCell colspan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+          }
           </TableBody>
           </Table>
       </Paper>
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-            {
-              'id': 1,
-              'image': 'https://placeimg.com/64/64/1',
-              'name': '홍리나',
-              'birthday': '20021010',
-              'gender': 'femail',
-              'job': 'student'
-            },
-            {
-              'id': 2,
-              'image': 'https://placeimg.com/64/64/2',
-              'name': '홍나리',
-              'birthday': '20021010',
-              'gender': 'femail',
-              'job': 'student'
-            },
-            {
-              'id': 3,
-              'image': 'https://placeimg.com/64/64/3',
-              'name': '홍국일',
-              'birthday': '2002101011',
-              'gender': 'femail',
-              'job': 'doctor'
-            }
-      ]);
+    //res.send();
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
